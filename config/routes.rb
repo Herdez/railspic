676931d261f2f426f88369ae721bf6c69ac438a3
Herdez <abelquantikum@gmail.com>
2015-02-07 23:32:24 -0600

    Create static page in app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+
   resources :ideas
 
   root :to => redirect('/ideas')
+
+  get "pages/info"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
